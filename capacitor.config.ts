b5c26bd26e9951b1357af5ab7ffe8afe40ef1fd1
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2024-08-08 16:31:39 -0300

    feat: adicionado o capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.firestore.app',
+  appName: 'firestore-app',
+  webDir: 'build'
+};
+
+export default config;
