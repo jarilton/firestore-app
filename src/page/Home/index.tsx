e73ec95a1e77129fe12ca228843e65016653c081
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2024-08-09 01:17:37 -0300

    feat: adicionado as funções para editar e remover as mensagens enviadas e adicionado icones fontawesome

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { collection, addDoc, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  onSnapshot,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../../firabase/firabase";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+import Lottie from "react-lottie";
+import * as animationData from "../../assets/lotties/not-found.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faSave, faTrash } from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./home.module.scss";
 
 interface Item {
@@ -13,6 +25,14 @@ interface Item {
 export const Home: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
   const [items, setItems] = useState<Item[]>([]);
+  const [editingItemId, setEditingItemId] = useState<string | null>(null);
+  const [editValue, setEditValue] = useState<string>("");
+
+  const LottieOptions = {
+    animationData,
+    loop: true,
+    autoplay: true,
+  };
 
   const handleAddItem = async () => {
     if (inputValue.trim() !== "") {
@@ -29,8 +49,31 @@ export const Home: React.FC = () => {
     }
   };
 
+  const handleDeleteItem = async (id: string) => {
+    try {
+      await deleteDoc(doc(db, "items", id));
+      console.log("Item deletado:", id);
+    } catch (error) {
+      console.error("Erro ao deletar item:", error);
+    }
+  };
+
+  const handleEditItem = async (id: string) => {
+    try {
+      await updateDoc(doc(db, "items", id), { text: editValue });
+      setEditingItemId(null);
+      setEditValue("");
+    } catch (error) {
+      console.error("Erro ao editar item:", error);
+    }
+  };
+
   const handleInputChange = (value: string) => {
     setInputValue(value);
+  };
+
+  const handleEditChange = (value: string) => {
+    setEditValue(value);
   };
 
   useEffect(() => {
@@ -85,13 +128,70 @@ export const Home: React.FC = () => {
             <span className={styles.titleMessage}>Mensagens Enviadas</span>
           </div>
           <div className={styles.cardMessage}>
-            {items.map((item) => {
-              return (
-                <span className={styles.message} key={item.id}>
-                  {item.text}
-                </span>
-              );
-            })}
+            {items.length > 0 ? (
+              items.map((item) => (
+                <div className={styles.message} key={item.id}>
+                  <div>
+                    {editingItemId === item.id ? (
+                      <Input
+                        label=""
+                        value={editValue}
+                        onChange={handleEditChange}
+                        placeholder="Editando mensagem"
+                      />
+                    ) : (
+                      <span>{item.text}</span>
+                    )}
+                  </div>
+                  <div className={styles.buttonContainer}>
+                    {editingItemId === item.id ? (
+                      <button
+                        className={styles.saveButton}
+                        onClick={() => handleEditItem(item.id)}
+                      >
+                        <FontAwesomeIcon
+                          icon={faSave}
+                          color="white"
+                          size="1x"
+                        />
+                      </button>
+                    ) : (
+                      <button
+                        className={styles.editButton}
+                        onClick={() => {
+                          setEditingItemId(item.id);
+                          setEditValue(item.text);
+                        }}
+                      >
+                        <FontAwesomeIcon
+                          icon={faEdit}
+                          color="white"
+                          size="1x"
+                        />
+                      </button>
+                    )}
+                    <button
+                      className={styles.deleteButton}
+                      onClick={() => handleDeleteItem(item.id)}
+                    >
+                      <FontAwesomeIcon icon={faTrash} color="white" size="1x" />
+                    </button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className={styles.boxLottie}>
+                <Lottie
+                  options={LottieOptions}
+                  width="10rem"
+                  height="10rem"
+                  style={{
+                    margin: "none",
+                  }}
+                />
+                <span>Ops! Nenhuma mensagem enviada</span>
+              </div>
+            )}
           </div>
         </div>
       </div>
